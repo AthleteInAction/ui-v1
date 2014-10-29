803c1d42babc1ee4a17b1f8ac46ce017e6102995
Will Robinson <will@wambl.com>
2014-10-29 08:27:28 -0700

    NEW UI

--- ui-v1/ViewController.swift
+++ ui-v1/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    @IBOutlet weak var awaySelGD: UIView!
+    // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    
+    
+    
+    // ------------------------------------------------------
+    // ------------------------------------------------------
     var colors = [
         "darkGrey": UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1)
     ]
@@ -22,36 +32,51 @@ class ViewController: UIViewController {
     var field: FieldClass!
     
     var downs = [
-        [1,"1st Down"],
-        [2,"2nd Down"],
-        [3,"3rd Down"],
-        [4,"4th Down"],
-        ["kickoff","Kickoff"],
+        [1,"1ST"],
+        [2,"2ND"],
+        [3,"3RD"],
+        [4,"4TH"],
+        ["kickoff","KICKOFF"],
         ["pat","PAT"]
     ]
+    // ------------------------------------------------------
+    // ------------------------------------------------------
     
+    
+    
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        addNumberSelectors()
         
     }
     
     override func viewDidAppear(animated: Bool) {
-       
         
         
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-    func testA(){
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    
+    
+    // Add Number Selectors
+    // ======================================================
+    // ======================================================
+    func addNumberSelectors(){
         
-        NSLog("AAAA")
+        awayNumbers = NumberSelector(frame: CGRect(x: awaySelGD.center.x-(awaySelGD.bounds.width/2), y: awaySelGD.center.y-(awaySelGD.bounds.height/2), width: awaySelGD.bounds.width, height: awaySelGD.bounds.height), pview: self)
+        view.addSubview(awayNumbers)
         
     }
+    // ======================================================
+    // ======================================================
     
 }
